436514b259807d402bfde219b616dbceccf37ce1
Gabriel Laporte <gabrielblpp@gmail.com>
2024-12-02 10:19:06 -0300

    feat: database configuration and module

---
+++ src/shared/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+        logging: true,
+      })
+    })
+  ]
+})
+export class DatabaseModule {}
