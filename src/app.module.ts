0b412c827979d41f646d09ed03a099d329bdfdf6
Gabriel Laporte <gabrielblpp@gmail.com>
2024-12-02 14:09:04 -0300

    feat: wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './shared/database/database.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/presentation/controllers/auth.controller.ts
+++ src/auth/presentation/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthenticateDto } from '../dtos';
 import { AuthenticateUseCase } from '../../application/use-cases';
+import { JwtAuthGuard } from '../../infra/guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() data: AuthenticateDto) {
     return await this.authenticateUseCase.execute(data);
+  }
+
+  @Get('/teste')
+  @UseGuards(JwtAuthGuard)
+  async test() {
+    console.log('a');
   }
 }
