98ac6caf4a1331ff309f56bde22b3ef768c2fe53
Gabriel Laporte <gabrielblpp@gmail.com>
2024-12-03 18:46:54 -0300

    fix: typo

--- src/user/presentation/controllers/user.controller.ts
+++ src/user/presentation/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from '../dtos';
 import { CreateUserUseCase } from '../../application/use-cases';
-import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { ApiResponse } from '@nestjs/swagger';
 import { UserCreatedDTO } from '../dtos/response/user-created.dto';
 
 @Controller('users')
@@ -9,7 +9,6 @@ export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @Post()
-  @ApiBearerAuth()
   @ApiResponse({ status: 201, type: UserCreatedDTO })
   async createUser(@Body() data: CreateUserDTO): Promise<UserCreatedDTO> {
     const user = await this.createUserUseCase.execute(data);
